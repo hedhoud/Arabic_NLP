256abb6a183c76ac60b573927932e9777808af74
Naoura <hnaoura@koios.gso.lan>
2023-02-16 16:26:42 +0100

    modif in dataset split script

--- dataset_split.sh
+++ dataset_split.sh
@@ -3,15 +3,17 @@
 input=$1
 output=$2
 
-cat $input | shuf > shufl_all.txt 
+cat $input | sort | uniq | shuf > $output/shufl_all.txt 
 
 mkdir -p $output/all $output/train $output/val $output/test
 cp $input $output/all/all.text
 
-split -l $[ $(wc -l shufl_all.txt | xargs| cut -d" " -f1) * 70/100 ] $input $output/segment
+split -l $[ $(wc -l $output/shufl_all.txt | xargs| cut -d" " -f1) * 70/100 ] $output/shufl_all.txt $output/segment
 mv $output/segmentaa $output/train/train.txt
 split -l $[ $(wc -l $output/segmentab | xargs| cut -d" " -f1) * 70/100 ] $output/segmentab $output/seg
+
 mv $output/segaa $output/test/test.txt
 mv $output/segab $output/val/val.txt
+
 rm $output/seg*
-rm shufl_all.txt
+rm $output/shufl_all.txt
